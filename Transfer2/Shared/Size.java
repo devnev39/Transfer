5c55b9fa1a3728a19e819b832fbd1317143bd302
Dhruv Bonde <devnev39@gmail.com>
2022-01-20 21:58:29 +0530

    size of a folder

--- Transfer2/Shared/Size.java
+++ Transfer2/Shared/Size.java
@@ -17,6 +17,9 @@ public class Size extends Command {
         super("size", cHandler,root);
     }
 
+    private long size = 0;
+    private long count = 0;
+
     @Override
     public void executeClientProcedure(String input, Socket serverSocket) throws Exception {
         String[] vals = remove(input.split(" "),0);
@@ -31,7 +34,13 @@ public class Size extends Command {
                 long len = this.ReceiveInputDataLenght(serverSocket);
                 float meg = (float)len / 1048576.0f;
                 this.clientEventHandler.TextReceived(val+"\t"+meg+" MB");
-            }else{
+            }else
+            if(flag==2){
+                long len = this.ReceiveInputDataLenght(serverSocket);
+                float size = (float)this.ReceiveInputDataLenght(serverSocket) / 1048576.0f;
+                this.clientEventHandler.TextReceived("Files : "+len+" Total Size : "+size+" MiB");
+            }
+            else{
                 this.clientEventHandler.FileNotExistServerError(val);
             }
         }
@@ -42,11 +51,24 @@ public class Size extends Command {
         String[] inp = remove(input.split(" "),0);        
         String val = String.join(" ", inp);
         File f = new File(this.Root+File.separator+val);
-        if(f.exists()){
+        if(f.exists() && f.isFile()){
             long len = f.length();
             clientSocket.getOutputStream().write(ByteBuffer.allocate(4).putInt(1).array());
             clientSocket.getOutputStream().write(ByteBuffer.allocate(8).putLong(len).array());
-        }else{
+        }else
+        if(f.exists() && f.isDirectory()){
+            this.size = 0;
+            this.count = 0;
+            this.GetToLastFiles(f.getPath()).forEach((file)->{
+                File s = new File(file);
+                this.size += s.length();
+                this.count++;
+            });
+            clientSocket.getOutputStream().write(ByteBuffer.allocate(4).putInt(2).array());
+            clientSocket.getOutputStream().write(ByteBuffer.allocate(8).putLong(this.count).array());
+            clientSocket.getOutputStream().write(ByteBuffer.allocate(8).putLong(this.size).array());
+        }
+        else{
             clientSocket.getOutputStream().write(ByteBuffer.allocate(4).putInt(-1).array());
         }
     }
